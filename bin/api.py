c2757b373f8e60908fc71d47b0a1c29d69b4f69a
Alistair Mersereau <alistairyves@gmail.com>
2014-04-18 18:16:25 -0400

    api update

--- bin/api.py
+++ bin/api.py
@@ -12,7 +12,7 @@ Function call executes an api call. It accepts three inputs:
 Function Returns
 The function will always return two elements.
 If the function failed, it will return first the return code, second the error message
-If the api call succeded, it will return first the reply from the api call, then the new last modified date
+If the api call succeeded, it will return first the reply from the api call, then the new last modified date
 If the api call replied saying that the address has not been modified, it will return first '1', then 'HTTP Error 304: Not Modified'
 """
     
