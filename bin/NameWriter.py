52747cdb57bcb18c17965dd6f1bd4d0fd60da527
Alistair Mersereau <alistairyves@gmail.com>
2014-04-18 21:19:30 -0400

    init files added to make directories

--- bin/NameWriter.py
+++ bin/NameWriter.py
@@ -2,6 +2,6 @@
 
 def write(battlename = None, battlenumber = None):
     if (type(battlename) != "String" ) or (type(battlenumber) != "String"):
-        raise Error("bad arguments")
+        return "Bad Arguments"
     
     
