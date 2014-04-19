39a5fc8571eebe1b77b86456c99d0f340af9055e
Alistair Mersereau <alistairyves@gmail.com>
2014-04-18 22:59:08 -0400

    some successful tests

--- test/NameWriterTest.py
+++ test/NameWriterTest.py
@@ -1,19 +1,19 @@
 import unittest
 import os
+import sys
+sys.path.append('/home/alistair/git/D3GET/bin')
+import NameWriter
 
-#import bin.NameWriter
-#import D3GET.bin.NameWriter
-from .bin.NameWriter import NameWriter
 print "1"
 
 class NameWriterTest(unittest.TestCase):
     print "2"
     
     def testNameWrite_write_bad_test(self):
-        self.assertEquals(NameWriter(12,12), "BadArguments")
+        self.assertEquals(NameWriter.write(12,12), "Bad Arguments")
         
-def main():
-    unittest.main()
+#def main():
+    #unittest.main()
     
-if __name__ == '__main__':
-    main()
+#if __name__ == '__main__':
+    #main()
