d88d54a81d944f3ffa60ed9d7d493b5dcb783ecf
Alistair Mersereau <alistairyves@gmail.com>
2014-04-18 20:31:10 -0400

    new, hoping to add a name writer to post names that have already been looke up

--- bin/NameWriter.py
+++ bin/NameWriter.py
@@ -1,1 +1,7 @@
-class NameWriter:
+
+
+def write(battlename = None, battlenumber = None):
+    if (type(battlename) != "String" ) or (type(battlenumber) != "String"):
+        raise Error("bad arguments")
+    
+    
