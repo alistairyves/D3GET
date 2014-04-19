d76f40c01c8fe2fca1b55deb0610112f5c87de3c
Alistair Mersereau <alistairyves@gmail.com>
2014-04-19 12:11:32 -0400

    2 tests pass

--- bin/NameWriter.py
+++ bin/NameWriter.py
@@ -1,7 +1,9 @@
 
 
 def write(battlename = None, battlenumber = None):
-    if (type(battlename) != "String" ) or (type(battlenumber) != "String"):
+    if (type(battlename) != str ) or (type(battlenumber) != str):
         return "Bad Arguments"
+    elif len(battlenumber) != 4:
+        return 'invalid battlenumber length'
     
     
--- test/NameWriterTest.py
+++ test/NameWriterTest.py
@@ -1,19 +1,19 @@
 import unittest
 import os
 import sys
-sys.path.append('/home/alistair/git/D3GET/bin')
+sys.path.append('/home/alistair/git/D3GET/bin/')
 import NameWriter
 
-print "1"
 
 class NameWriterTest(unittest.TestCase):
-    print "2"
     
-    def testNameWrite_write_bad_test(self):
-        self.assertEquals(NameWriter.write(12,12), "Bad Arguments")
+    def testNameWrite_write_notstrings(self):
+        self.assertEquals(NameWriter.write(12,12), 'Bad Arguments')
         
-#def main():
-    #unittest.main()
+    def testNameWrite_write_badnumber(self):
+        self.assertEquals(NameWriter.write('blah', '12344'), 'invalid battlenumber length')
+def main():
+    unittest.main()
     
-#if __name__ == '__main__':
-    #main()
+if __name__ == '__main__':
+    main()
